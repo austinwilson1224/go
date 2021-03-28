0c2e70295dcc5c4fe177533e1d2f765e98b9a6b1
austinwilson1224 <austin1224@gmail.com>
2021-03-27 18:54:14 -0700

    more go

---
+++ tutorial_edge/go.mod
@@ -0,0 +1,5 @@
+module github.com/go/tutorial_edge
+
+go 1.16
+
+require github.com/googollee/go-socket.io v1.4.4 // indirect
--- tutorial_edge/main.go
+++ tutorial_edge/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
 
+	socketio "github.com/googollee/go-socket.io"
+)
 func main() {
 	fmt.Println("test")
+
+	server, err := socketio.NewServer(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server.OnEvent("connection", func(so socketio.Socket) {
+		log.Println("New Connection")
+	})
+
+	http.Handle("/socket.io/", server)
+	log.Fatal(http.ListenAndServe(":5000", nil))
 }
