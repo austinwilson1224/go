a9dea21c1a1d3af1e0b31c6fd82d41a76d2aec66
austinwilson1224 <austin1224@gmail.com>
2024-08-08 21:55:33 -0700

    get req

--- 1.7-web-server/server.go
+++ 1.7-web-server/server.go
@@ -1,7 +1,23 @@
 package main 
 
-import "fmt"
+import (
+
+"fmt"
+"net/http"
+"log"
+
+)
 
 func main() {
 	fmt.Println("test")
+
+	http.HandleFunc("/", handler) // each request calls handler 
+
+
+	log.Fatal(http.ListenAndServe("localhost:8082", nil))
+
+}
+
+func handler(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "URL.path = %q\n", request.URL.Path)
 }
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # go
+
+'''go mod tidy'''
+## multi-modal workspaces 
+'''cd /top/level/dir
+
+go work init
+go work use project1
+go work use project2'''
+
+[multi module worspaces](https://stackoverflow.com/questions/65748509/vscode-shows-an-error-when-having-multiple-go-projects-in-a-directory)
+
+-- need to upgrade to 1.18+
